2936efe0b0132fe965e0446142c0998500dce987
SparkeEcrash <jpark121989@gmail.com>
2019-01-31 13:46:51 -0800

    finished setting up video streaming feature

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
-    componentDidMount = () => {
-        this.props.fetchStream(this.props.match.params.id);
+    constructor(props) {
+        super(props);
+
+        this.videoRef = React.createRef();
     }
-    
+
+    componentDidMount = () => {
+        const { id } = this.props.match.params;
+
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        } else {
+            const { id } = this.props.match.params;
+            this.player = flv.createPlayer({
+                type: 'flv',
+                url: `http://localhost:8000/live/${id}.flv`
+            });
+            this.player.attachMediaElement(this.videoRef.current);
+            //buildPlayer() is necessary because if this runs in componentdidmount this.videoRef does not exist because only <div>Loading...</div> is returned in render since this.props.stream is undefined
+            this.player.load();
+            this.player.play();
+        }
+    }
+
     render() {
         if(!this.props.stream) {
             return <div>Loading...</div>
@@ -16,6 +50,7 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
